0560fc20c2a2fb6db20ea8132e7f6c08c6957833
droglenc <dogle@northland.edu>
2015-10-17 13:52:59 -0500

    Added NoPrint version of book

--- zzzRestrictPrinting.R
+++ zzzRestrictPrinting.R
@@ -1,15 +1,15 @@
-## A script that runs IntroStats.pdf through pdftk.exe to
+## A script that runs Biometry.pdf through pdftk.exe to
 ## remove the ability to print the document, and saves the
-## result in IntroStats_noPrint.pdf.  The user is allowed to
+## result in Biometry_noPrint.pdf.  The user is allowed to
 ## copy contents to the clipboard.
 
 ## This creates the pdftk command line code.
-dir <- "C:/aaaWork/Web/GitHub/NCMTH107/book/"
-fn <- "IntroStats"
+dir <- "C:/aaaWork/Web/GitHub/NCMTH207/book/"
+fn <- "Biometry"
 eng <- paste0(dir,"pdftk.exe")
 inp <- paste0(dir,fn,".pdf")
 out <- paste0("output ",dir,fn,"_noPrint.pdf")
-pw <- paste("owner_pw mth107")
+pw <- paste("owner_pw mth207")
 allows <- "allow CopyContents"
 ( tmp <- paste(eng,inp,out,pw,allows,"verbose") )
 
