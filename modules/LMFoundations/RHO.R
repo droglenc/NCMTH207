c8d4bd28873df8716e3f98ce954ec64de9b53368
Derek Ogle <dogle@northland.edu>
2017-01-12 15:32:51 -0600

    Updated LMFoundations HO

--- modules/LMFoundations/RHO.R
+++ modules/LMFoundations/RHO.R
@@ -14,21 +14,21 @@ library(NCStats)
 setwd("C:/aaaWork/Web/GitHub/NCMTH207/modules/LMFoundations")
 sdir <- read.csv("SexDirection.csv")
 str(sdir)
-sdir$fsex <- factor(sdir$sex,levels=c("male","female"))
-levels(sdir$fsex)
+sdir$sexmf <- factor(sdir$sex,levels=c("male","female"))
+levels(sdir$sexmf)
 levels(sdir$sex)
 
-hist(abserr~fsex,data=sdir,xlab="Absolute Error",breaks=seq(0,200,25),col="gray90")
-Summarize(abserr~fsex,data=sdir,digits=2)
-levenesTest(abserr~fsex,data=sdir)
-t.test(abserr~fsex,data=sdir,var.equal=TRUE)
+hist(abserr~sexmf,data=sdir,xlab="Absolute Error",w=25)
+Summarize(abserr~sexmf,data=sdir,digits=2)
+levenesTest(abserr~sexmf,data=sdir)
+t.test(abserr~sexmf,data=sdir,var.equal=TRUE)
 t.test(abserr~sex,data=sdir,var.equal=TRUE)
 
-lm1 <- lm(abserr~fsex,data=sdir)
+lm1 <- lm(abserr~sexmf,data=sdir)
 summary(lm1)
 confint(lm1)
 fitPlot(lm1,xlab="Sex",ylab="Absolute Error",main="")
 anova(lm1)
 
 
-# Script created at 2016-12-28 06:34:35
+# Script created at 2017-01-12 15:32:15
