5be7144aa5f8822b2ad43cc1f5956f0eac8bdb73
Derek Ogle <droglenc@users.noreply.github.com>
2018-12-08 19:43:29 -0600

    Changed IVR HO and Slides

--- modules/IVRegression/RHO.R
+++ modules/IVRegression/RHO.R
@@ -1,41 +1,42 @@
 library(NCStats)
 setwd("C:/aaaWork/Web/GitHub/NCMTH207/modules/IVRegression")
 
-ss <- read.csv("SalmonSperm.csv")[-c(1,10,11),]  # only for class demo purposes
-str(ss)
-xlbl <- "Sperm Tail End Piece Length (um)"
-ylbl <- "Fertilization Success"
+df <- read.csv("LaysanTeal.csv")
+str(df)
+xlbl <- "Maximum Count"
+ylbl <- "Lincoln-Petersen Abundance"
 
-lm1 <- lm(fert.succ~step.len*mat,data=ss)
+lm1 <- lm(mr.estimate~max.count*season,data=df)
 
 transChooser(lm1)
 
-summary(lm1)
-confint(lm1)
-fitPlot(lm1,interval="confidence",xlab=xlbl,ylab=ylbl,legend="topleft")
-( nd <- data.frame(step.len=c(3.4,3.7),mat=c("Adult","Parr")) )
-predictionPlot(lm1,nd,interval="confidence",legend="topleft")
-
-lm1 <- lm(fert.succ~step.len*mat,data=ss)
+lm1 <- lm(mr.estimate~max.count*season,data=df)
 anova(lm1)
 
 tmp <- anova(lm1)
 attr(tmp,"heading") <- NULL
 tmp
 
-lm2 <- lm(fert.succ~step.len+mat,data=ss)
+lm2 <- lm(mr.estimate~max.count+season,data=df)
 anova(lm2)
 
 tmp <- anova(lm2)
 attr(tmp,"heading") <- NULL
 tmp
 
-lm3 <- lm(fert.succ~step.len,data=ss)
+lm3 <- lm(mr.estimate~max.count,data=df)
 anova(lm3)
 
 tmp <- anova(lm3)
 attr(tmp,"heading") <- NULL
 tmp
+
+fitPlot(lm1,interval="confidence",xlab=xlbl,ylab=ylbl,legend="topleft",cex.leg=0.7)
+summary(lm1)
+confint(lm1)
+( nd <- data.frame(max.count=c(250,250),season=c("breeding","non-breeding")) )
+predictionPlot(lm1,nd,interval="confidence",xlab=xlbl,ylab=ylbl,ylim=c(300,900),
+               legend="topleft",cex.leg=0.7)
 
 FED <- read.csv("FishEnergyDensity.csv")
 str(FED)
@@ -46,8 +47,12 @@ transChooser(lm1)
 
 anova(lm1)
 
+tmp <- anova(lm1)
+attr(tmp,"heading") <- NULL
+tmp
+
 compSlopes(lm1)
-fitPlot(lm1,xlab="Dry Weight",ylab="Energy Density",legend="topleft")
+fitPlot(lm1,xlab="Dry Weight",ylab="Energy Density",legend="topleft",cex.leg=0.8)
 
 FED1 <- filterD(FED,species!="bayanchovy")
 lm2 <- lm(ed~dw*species,data=FED1)
@@ -59,7 +64,7 @@ tmp
 
 compIntercepts(lm2)
 compIntercepts(lm2,common.cov=0)
-fitPlot(lm2,xlab="Dry Weight",ylab="Energy Density",legend="topleft")
+fitPlot(lm2,xlab="Dry Weight",ylab="Energy Density",legend="topleft",cex.leg=0.8)
 
 
-# Script created at 2018-02-18 13:12:08
+# Script created at 2018-12-08 19:42:45
